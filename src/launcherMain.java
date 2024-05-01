1be741634fa52b760e1d59421557311c67de8bd0
lirongrong <lirongrong202202@163.com>
2024-05-01 23:43:50 +0800

    up to code

--- src/launcherMain.java
+++ src/launcherMain.java
@@ -1,26 +1,8 @@
-import java.util.Scanner;
-
 /**
  * 启动主方法
  */
 public class launcherMain {
-    //获取用户名和密码
-    public static Object getUsernameAndPassword(){
-        Scanner $$username = new Scanner(System.in);
-        System.out.print("请输入用户名：");
-        String $username = $$username.nextLine();
-        Scanner $$password = new Scanner(System.in);
-        System.out.print("请输入密码：");
-        String $password = $$password.nextLine();
-        if($username!="" & $password!=""){
-            String[] $user = {$username,$password};
-            return $user;
-        }else {
-            System.out.println("请重新输入账号和密码！");
-            return getUsernameAndPassword();
-        }
-    }
     public static void main(String[] args) {
-        getUsernameAndPassword();
+        api.getUsernameAndPassword();
     }
 }

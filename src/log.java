df9d84f40db410c5aa0b2703872db1462d223bf4
lirongrong <lirongrong202202@163.com>
2024-05-15 10:51:05 +0800

    ***

--- src/log.java
+++ src/log.java
@@ -4,24 +4,29 @@ import java.io.IOException;
 public class log extends Thread{
     public static boolean initLog() {
         try {
+            //获取时间
             String $date = api.Date().split(" ")[0];
             File logfile = new File("../MSRCLauncher/log", $date + "log.log");
+            //判断日志文件是否存在
             if (logfile.exists()) {
             } else {
                 try {
                     logfile.createNewFile();
                 } catch (IOException e) {
+                    //抛出错误
                     byte errorByte = value.IOException;
                     api.Error(errorByte);
                     return false;
                 }
             }
+            //返回执行成功
             return true;
         }catch (Exception e){
+            //返回执行失败
             return false;
         }
     }
     public static void main(String[] args) {
-
+        //TODO
     }
 }

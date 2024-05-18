b657fbd9ba8148d14abcf24285671bd1dd4f5ff5
lirongrong <lirongrong202202@163.com>
2024-05-18 17:03:30 +0800

    update for code

--- src/api.java
+++ src/api.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.Date;
 public class api extends mapi{
@@ -11,9 +13,9 @@ public class api extends mapi{
     //登录
     public static boolean login(){
         try {
-            mapi.initPlugins();
+            //mapi.initPlugins();
         }catch (Exception e){
-            initPlugins();
+            //initPlugins();
         }
         return false;
     }
@@ -21,7 +23,7 @@ public class api extends mapi{
     public void initPlugins(){
     }
     //获取用户名和密码
-    public static Object getUsernameAndPassword(){
+    public static final Object getUsernameAndPassword(){
         Scanner $$username = new Scanner(System.in);
         System.out.print("请输入用户名：");
         String $username = $$username.nextLine();
@@ -54,7 +56,7 @@ public class api extends mapi{
         }
     }
     //获取时间
-    public static String Date(){
+    public static final String Date(){
         Date d = new Date();
         SimpleDateFormat sbf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sbf.format(d);
@@ -72,4 +74,65 @@ public class api extends mapi{
         }
     }
 
+    //debug方法
+    protected static void debug(){
+        //死循环交互
+        for(;;){
+            Scanner $input = new Scanner(System.in);
+            System.out.println("################################################################");
+            System.out.println();
+            System.out.println("DEBUG MSRC INDEV 1.0 NOT LTS");
+            System.out.println();
+            System.out.println("################################################################");
+            System.out.println("成功进入debug模式！使用-help命令获取使用方法！");
+            //获取输入
+            String $tmp = $input.nextLine();
+            if(Objects.equals($tmp, "-var")){
+                System.out.println("#################################################################");
+                System.out.println("变量名"+"\t\t"+"变量解释"+"\t\t\t\t  "+"变量值类型"+"\t变量值");
+                String[] $var = {"$$loginit","$$clientinit"};
+                System.out.println("$$loginit"+"  "+"日志系统初始化状态"+"\tboolean"+"   "+launcherMain.$$loginit);
+                System.out.println("$$clientinit"+"客户端主线程初始化状态"+"\tboolean"+"   "+launcherMain.$$clientinit);
+                System.out.println("#################################################################");
+                /*TODO 循环输出变量
+                for (int i=0;i<$var.length;i++){
+                    //循环输出变量
+                    //缓存变量
+                    int p = i;
+                    String y =$var[p];
+                    String c ="launcherMain."+y;
+                    System.out.println("$$loginit"+"  "+"日志系统初始化状态"+"boolean"+"   "+c);
+                }
+                */
+                /*TODO 长度优化输出
+                   for (int i=0;i<$var.length;i++){
+                    //如果长度小于十个字符
+                    if ($var[i].length() <= 10) {
+                        //直接输出
+                        System.out.print($var[i]);
+                    }else {
+                        //对长度超过十个字符的变量进行处理处理
+                        int j= ($var[i].length())/2;
+                        String[] tmp = {}
+                    }
+                }
+                 */
+            } else if (Objects.equals($tmp,"-exit")) {
+                //退出debug模式
+                launcherMain.main(new String[]{"debug"});
+            } else if (Objects.equals($tmp,"-kill")) {
+                //退出程序
+                System.exit(0);
+            } else if (Objects.equals($tmp,"-help")) {
+                //文档内容
+                System.out.println("-var 查看变量追踪");
+                System.out.println();
+                System.out.println("-exit 退出debug模式");
+                System.out.println();
+                System.out.println("-kill 结束程序(包括子线程以及主线程)");
+                System.out.println();
+                System.out.println("-help 获取使用文档");
+            }
+        }
+    }
 }
--- src/launcherMain.java
+++ src/launcherMain.java
@@ -1,18 +1,22 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
 
 /**
  * 启动主方法
  */
 
 public class launcherMain {
-    static {
-        boolean $$loginit = log.initLog();
-        boolean $$clinetinit = api.initClient();
+    public static boolean $$loginit = false;
+    public static boolean $$clientinit = false;
+    static{
+        $$loginit = log.initLog();
+        $$clientinit = api.initClient();
         if ($$loginit == false) {
             System.out.println("日志系统初始化失败");
-            if ($$clinetinit == false) {
+            if ($$clientinit == false) {
                 System.out.println("客户端初始化失败，已创建错误文件。");
                 File $file = new File("error.txt");
                 try {
@@ -20,15 +24,33 @@ public class launcherMain {
                     FileWriter fileWriter = new FileWriter($file);
                     fileWriter.write("尝试创建必要文件是失败！");
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
                 }
-                System.gc();
-                System.exit(50);
+                //System.gc();
+                //System.exit(50);
             }
         }
     }
 
     public static void main(String[] args) {
-
+        //判断启动参赛长度是否为零
+        if(args.length!=0){
+            //判断启动参赛是否为"debug"
+            if (args[0]!="debug");
+            {
+                for(;;){
+                    Scanner $$input = new Scanner(System.in);
+                    System.out.print("请问如否进入DEBUG模式(Y/N)");
+                    String $$tmp = $$input.next();
+                    if (Objects.equals($$tmp, "Y")) {
+                        api.debug();
+                        break;
+                    } else if (Objects.equals($$tmp, "N")) {
+                        break;
+                    }else {
+                        System.out.println("请输入Y 或 N ！");
+                    }
+                }
+            }
+        }
     }
 }
